7f0cc25d593c1d901b630ef181cf1bb978b10c11
MisuStefanLeonard <aade211@yahoo.com>
2024-05-28 11:44:56 +0300

    Updated version

--- ESPCONTROL/src/main/java/JavaESPControl/Service/ThermometerServiceImplementation.java
+++ ESPCONTROL/src/main/java/JavaESPControl/Service/ThermometerServiceImplementation.java
@@ -13,10 +13,7 @@ import java.io.InputStreamReader;
 import java.math.BigDecimal;
 import java.net.Socket;
 import java.sql.Date;
-import java.time.LocalDate;
 import java.util.concurrent.CompletableFuture;
-import java.util.logging.Level;
-import java.util.logging.Logger;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.scheduling.annotation.Async;
 import org.springframework.stereotype.Service;
@@ -38,7 +35,7 @@ public class ThermometerServiceImplementation implements ThermometerService {
         return CompletableFuture.supplyAsync(() -> {
             try {
                 espConn = new ESP8266connection();
-                System.out.println("aici1212323");
+               
                 return espConn.getConnection();
             } catch (IOException e) {
                 throw new RuntimeException("Error while getting connecting", e);
@@ -51,44 +48,33 @@ public class ThermometerServiceImplementation implements ThermometerService {
             try {
                 boolean messageFlag = true;
                 String OUTmessage = "";
-                System.out.println("inainte de loop in getMessageFromESP");
                 while(messageFlag){
                     StringBuilder messageBuild = new StringBuilder();
-                    //System.out.println(" in loop getMessageFromESP");
+                   
                     InputStreamReader input = new InputStreamReader(connSocket.getInputStream());
-                    //System.out.println(" in loop getMessageFromESP linia 58");
+                    
                     BufferedReader bf_reader = new BufferedReader(input);
-                    //System.out.println(" in loop getMessageFromESP linia 59");
-//                    while(bf_reader.read() >= 0 && bf_reader.read() <= 65535){
-//                        System.out.println("inainte de ready");
-//                        bf_reader.ready();
-//                        System.out.println("dupa ready");
-//                        int INTcharacter = bf_reader.read();
-//                        char character = (char) INTcharacter;
-//                        messageBuild.append(character);
-//                        System.out.println(messageBuild);
-//                    }
+                    
                     boolean readerFlag = true;
-                    while(messageFlag){
+                    while(readerFlag){
                         try{
                             if(bf_reader.ready()){
-                                System.out.println("aici la ready");
+                               
                                 String messageReceived = bf_reader.readLine();
-                                System.out.println("dupa readline");
+                               
                                 messageBuild.append(messageReceived);
                                 System.out.println(messageBuild.toString());
-                                messageFlag = false;
+                                readerFlag = false;
                             }
                         }catch(IOException e){
                             e.printStackTrace();
-                            messageFlag = true;
+                            readerFlag = true;
                         }
                     }
                     
-                    System.out.println("mesajul -> " + messageBuild.toString());
-                    System.out.println("am ajuns aici");
+                   
                     String message = messageBuild.toString();
-                    System.out.println("citeste linia in getMessageFromESP");
+                   
                     if(message.isEmpty()){
                         System.out.println("empty message");
                     }else{
@@ -108,13 +94,13 @@ public class ThermometerServiceImplementation implements ThermometerService {
     public CompletableFuture<Void> save(ThermometerData data) throws IOException{
         return getAlreadyOpenedConnectionAsync().thenCompose(connSocket -> {
             try {
-                System.out.println("connection opened here");
+                
                 if (connSocket.isConnected()) {
-                    System.out.println("after isconnected condition ");
+                    
                     if (connSocket.isClosed()) {
                         throw new Error("Connection closed");
                     } else {
-                        System.out.println("after isconnected condition2 ");
+                        
                         return getMessageFromESP(connSocket).thenApply(message -> {
                             System.out.println(message);
                             ThermometerData dataToBeInserted = new ThermometerData();
